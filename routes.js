0abbc0157c27111a7f3fa10150cb7f3192c959a8
Ashley Cane <atcane@yahoo.com>
2021-03-10 18:58:05 -0500

    Updates

--- routes.js
+++ routes.js
@@ -2,14 +2,72 @@ const express = require('express');
 const routes = express.Router();
 const pool = require('./connection');
 // GET /routes
-routes.get("/conditions", (req, res) => {
+routes.get("/symptoms", (req, res) => {
     pool.query('SELECT * FROM conditions').then((result) => {
         res.json(result.rows)
     });
-})
+
+    if (req.query.prefix) {
+        pool.query('SELECT * FROM conditions LIKE $1', [req.query.prefix + "%"]).then((result) => {
+        res.json(result.rows)
+        });
+    }
+    else{
+        pool.query('SELECT * FROM conditions').then((result) => {
+            res.json(result.rows);
+            });
+    }
+});
+
 routes.get("/eo", (req, res) => {
         pool.query('SELECT * FROM ess_oils').then((result) => {
             res.json(result.rows)
         });
+    
+        if (req.query.prefix) {
+            pool.query('SELECT * FROM ess_oils LIKE $1', [req.query.prefix + "%"]).then((result) => {
+            res.json(result.rows)
+            });
+        }
+        else{
+            pool.query('SELECT * FROM ess_oils').then((result) => {
+                res.json(result.rows);
+                });
+        }
 });
     module.exports = routes;
+
+    
+    //    let maxPrice = parseFloat(req.query.maxPrice) 
+    //     if (maxPrice) {
+    //         pool.query('SELECT * FROM shopping_cart WHERE price <= $1', [maxPrice]).then((result) => {
+    //                 res.json(result.rows);
+    //         });
+    //     }
+        
+       
+    
+    //     if (req.query.pageSize) {
+    //         pool.query('SELECT * FROM shopping_cart LIMIT $1', [req.query.pageSize]).then((result) => {
+    //         res.json(result.rows)
+    //         });
+    //     }
+    //         else{
+    //             pool.query('SELECT * FROM shopping_cart').then((result) => {
+    //                 res.json(result.rows);
+    //                 });
+    //         }
+    // });
+    
+    // // GET /cart-items/:id
+    // cart.get("/:id", (req, res) => {
+    //    let id = parseInt(req.params.id);
+    //     pool.query('SELECT * FROM shopping_cart WHERE id = $1', [id]).then((result) => {
+    //         res.json(result.rows)
+    //     });
+    
+    //     // if (!id) res.status(404).send('ID Not Found')
+    // });
+    
+
+    
